d2ccd4b07e62d2aebacc9f71d3779ede4e9ccb2e
Kallyne Layane <hellookallyne@gmail.com>
2025-02-24 20:20:30 -0300

    Create the GameBoard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Header } from './components/Header';
+import { GameBoard } from './components/GameBoard';
 
 export const App = () => {
   return (
     <div className="app">
       <Header />
+      <GameBoard />
     </div>
   );
 };
---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,3 @@
+export const GameBoard = () => {
+  return <main className="game-board container">GameBoard</main>;
+};
